cdefa801f78cab81677f6d958a5a08fa98f065d4
Rayden Zhang <rayden3651@outlook.com>
2026-04-06 17:39:11 -0400

    Attempted evading filters

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,2 @@
-const games = document
-    .querySelector("#special")
-    .querySelectorAll("a");
-for (const el of games) {
-    if (!el.href.includes("asyncem"))
-        continue;
-    const link = el.href;
-    el.href = "about:blank"; // To give it styling
-    el.removeAttribute("target");
-    el.addEventListener("click", (e) => {
-        e.preventDefault();
-        const win = window.open("about:blank", "_blank");
-        if (!win)
-            return alert(`An unknown error occured.`);
-        const iframe = document.createElement("iframe");
-        iframe.src = link;
-        iframe.style.width = "99vw";
-        iframe.style.height = "99vh";
-        win.document.body.appendChild(iframe);
-    });
-}
-window.addEventListener("beforeunload", (e) => {
-    const msg = `Are you sure you want to leave?`;
-    e.returnValue = msg;
-    return msg;
-});
 export {};
 //# sourceMappingURL=index.js.map
